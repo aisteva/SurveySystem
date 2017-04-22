ce5404b2efccb9097615974484b75dc99dab4b6b
Artūras Fiodorovas <arturasfio@gmail.com>
2017-04-22 22:32:22 +0300

    arquillian test bandymas tfs'e

---
+++ src/test/java/services/EmailServiceTestJUnit.java
@@ -0,0 +1,19 @@
+package services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arturas on 2017-04-22.
+ */
+public class EmailServiceTestJUnit
+{
+    @Test
+    public void sendEmail() throws Exception
+    {
+        EmailService s = new EmailService();
+        s.sendEmail("test@example.com", "This is a test");
+    }
+
+}
