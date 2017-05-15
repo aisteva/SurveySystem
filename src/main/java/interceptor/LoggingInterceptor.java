70f5463bc51065f4617db7854717415dd43ee84f
Aiste <aiste.vaznelyte@gmail.com>
2017-05-15 17:50:49 +0300

    kurtas failas

--- src/main/java/interceptor/LoggingInterceptor.java
+++ src/main/java/interceptor/LoggingInterceptor.java
@@ -1,13 +1,22 @@
 package interceptor;
 
+import org.hibernate.Session;
+
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
+import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Aiste on 2017-05-15.
@@ -18,6 +27,10 @@ public class LoggingInterceptor implements Serializable {
 
     @Resource
     private SessionContext context;
+
+    @Inject private EntityManager entityManager;
+
+
 
     private static final long serialVersionUID = 813L;
     Date dateNow = new Date( );
@@ -33,15 +46,34 @@ public class LoggingInterceptor implements Serializable {
 
     @AroundInvoke
     public Object logMethodEntry(InvocationContext ctx) throws Exception {
-        String originName = Thread.currentThread().getName();
-//        String currentUser = context.getCallerPrincipal().getName();
-        try{
-//            String tracingName = currentUser + " " + originName;
-        System.out.println(formatedDate.format(dateNow)+" : Entering Class: " + ctx.getTarget().getClass().getName()+" METHOD: " +
-                ctx.getMethod().getName() + "    USER:: "+ originName);
-            return ctx.proceed();
-        }finally{
 
+
+        String originName = Thread.currentThread().getName();
+
+        Session session = entityManager.unwrap(Session.class);
+        System.out.println(session);
+
+
+
+
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter("C://Users/Aiste/Desktop/logOutput.txt", true);
+            System.out.println(System.getProperty("user.dir"));
+            pw = new PrintWriter(fw);
+
+            pw.println(formatedDate.format(dateNow)+" : Entering Class: " + ctx.getTarget().getClass().getName()+" METHOD: " +
+                    ctx.getMethod().getName() + "    USER:: ");
+
+            pw.close();
+            fw.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(ctx.getTarget().getClass().getName()).log(Level.SEVERE, null, ex);
         }
+
+
+        return ctx.proceed();
     }
 }
