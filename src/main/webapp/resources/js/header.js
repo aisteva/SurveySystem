882f551a6a7ed44157715c32f99bf3d3fea0b3d4
ambrazasp <p.ambrazas@gmail.com>
2017-05-25 21:11:10 +0300

    Apklausos nuorodos rodymas ir kopijavimas

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -7,4 +7,30 @@ $(document).ready(function() {
     $('#nav-icon').click(function(){
         $(this).toggleClass('collapsed');
     });
+
+    $('.link-copy').tooltip({
+        trigger: 'click',
+        placement: 'bottom'
+    });
+
+    function setTooltip(btn, message) {
+        btn.tooltip('hide')
+            .attr('data-original-title', message)
+            .tooltip('show');
+    }
+
+    function hideTooltip(btn) {
+        setTimeout(function() {
+            btn.tooltip('hide');
+        }, 1000);
+    }
+
+
+    var clipboard = new Clipboard('.link-copy');
+
+    clipboard.on('success', function(e) {
+        var btn = $(e.trigger);
+        setTooltip(btn, 'Nukopijuota');
+        hideTooltip(btn);
+    });
 });
