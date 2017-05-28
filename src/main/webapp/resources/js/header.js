1a9ac8fc32ad233d061cc8ac1061009edbeb5ed5
ambrazasp <p.ambrazas@gmail.com>
2017-05-28 21:24:11 +0300

    iškeltas kopijavimas į index.js failą (clipboard)

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -18,24 +18,4 @@ $(document).ready(function() {
        $(this).addClass('active');
     });
 
-    function setTooltip(btn, message) {
-        btn.tooltip('hide')
-            .attr('data-original-title', message)
-            .tooltip('show');
-    }
-
-    function hideTooltip(btn) {
-        setTimeout(function() {
-            btn.tooltip('hide');
-        }, 1000);
-    }
-
-
-    var clipboard = new Clipboard('.link-copy');
-
-    clipboard.on('success', function(e) {
-        var btn = $(e.trigger);
-        setTooltip(btn, 'Nukopijuota');
-        hideTooltip(btn);
-    });
 });
